6ad6443a4f2261a5a2eeeba180c8652a14aed484
stormpsons <mastarmynd@gmail.com>
2022-09-08 17:35:43 -0700

    jaye

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
-#include <unistd.h>
+#include <stdio.h>
+
 /**
-  * main - Entry point 
+  * main - Entry point
   * Return: Always 0 (Success)
   */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+			return (1);
 }
