de8799d6a2e34d0210256e456ca7088a3fb495be
stormpsons <mastarmynd@gmail.com>
2022-09-10 01:11:22 -0700

    adage

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,12 +2,12 @@
 
 /**
   * main - program that prints all possible different combinations of
-   * three digits
-    * Numbers must be separated by ,followed by a space
-     * The two digits must be different
-      * 012, 120, 102, 021, 201, 210 are considered the same combination
-       * Return: 0
-        */
+ * three digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * Return: 0
+ */
 
 int main(void)
 {
