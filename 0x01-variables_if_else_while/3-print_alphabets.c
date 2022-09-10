d1899c264e03e62638addd2d17e306e31d2e2899
stormpsons <mastarmynd@gmail.com>
2022-09-09 22:26:38 -0700

    yale

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char low;
+char low;
 
-	for (low = 'a'; low <= 'z'; low++)
-				putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-				putchar(low);
-				putchar('\n');
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
 
-	return (0);
+return (0);
 }
