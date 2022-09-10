181a4858a3ea96727fa21448cd9de02e6f44cebc
stormpsons <mastarmynd@gmail.com>
2022-09-09 23:45:02 -0700

    stage

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,8 +4,7 @@
   * main - program that prints all possible different combinations of two digits
    * Numbers must be separated by ,followed by a space
     * The two digits must be different
-     * 01 and 10 are considered the same combination of the two digits 0 and 1
-      * Return: 0
+       * Return: Always 0 (Success)
        */
 
 int main(void)
