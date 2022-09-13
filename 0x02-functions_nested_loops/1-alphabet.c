38bb46cc515ea6becf98fe9563963415fa1feb12
stormpsons <mastarmynd@gmail.com>
2022-09-13 16:39:14 -0700

    sla

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,12 @@
 *it prints in lowercase
 *Return: no return
 */
-
 void print_alphabet(void)
 {
 	int letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
-
 	{
 		_putchar (letter);
-
 	}
 	_putchar (10);
 }
