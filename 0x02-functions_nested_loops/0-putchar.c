c9d48b732880f0e6df39207b7f4a0b041fbac2a4
stormpsons <mastarmynd@gmail.com>
2022-09-13 05:20:48 -0700

    slate

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,15 @@
 
 int main(void)
 {
-	char school[10] = "_putchar";
+	char *sh = "_putchar";
 
-	int i;
-
-	for (i = 0; i < 8; i++)
+	while (*sh)
 	{
-		_putchar(school[i]);
+		_putchar(*sh);
+		sh++;
+			
 	}
-	_putchar(10);
+	_putchar('\n');
+
 	return (0);
 }
