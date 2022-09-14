393d0deb4c01489bf745ccc41761d04a1d2cdaff
stormpsons <mastarmynd@gmail.com>
2022-09-13 18:21:55 -0700

    try

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,18 +11,17 @@ unsigned long n1 = 0, n2 = 1, n3;
 		
 for (inc = 0; inc < 50; inc++)
 {
-	n3 = n1 + n2;
-	printf("%lu", n3);
-	n1 = n2;
-	n2 = n3;
+n3 = n1 + n2;
+printf("%lu", n3);
+n1 = n2;
+n2 = n3;
 
 if (inc == 49)
-	printf("\n");
+printf("\n");
 else
-	printf(", ");
+printf(", ");
 
-
-	}
+}
 
 return (0);
 }
