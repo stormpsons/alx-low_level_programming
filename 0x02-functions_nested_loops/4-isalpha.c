275388ea4f115cafb9fc9c629535204cd303c19d
stormpsons <mastarmynd@gmail.com>
2022-09-13 17:03:57 -0700

    yes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
  *_isalpha - controls if a character is alphabetical
  *@c: character to be verified
  *Return: return 0 or 1
- * */
+ */
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
