03f3b38129756e39a352b89c3f4f5558da85c793
stormpsons <mastarmynd@gmail.com>
2022-09-15 12:33:28 -0700

    soon

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 ifndef MAIN_H
 #define MAIN_H
 /**
- * *main - header file for protoypes
- * *void protoypes (void)
- * *int protoypes (int)
- * */
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
