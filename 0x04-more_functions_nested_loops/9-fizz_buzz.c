def20b68f56b231439368ccb88fcd5d3780eabf9
stormpsons <mastarmynd@gmail.com>
2022-09-15 14:37:59 -0700

    seal

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,30 @@ int main(void)
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
 			printf("FizzBuzz ");
+
 		}
 		else if ((num % 3) == 0)
 		{
 			printf("Fizz ");
+
 		}
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
 			{
 				printf("Buzz ");
+
 			}
 			else
 			{
 				printf("Buzz");
+
 			}
 		}
 		else
 		{
 			printf("%d ", num);
+
 		}
 	}
 	printf("\n");
