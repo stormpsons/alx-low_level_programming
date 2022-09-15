c63c197442cb76a9ef05d17c76b50b6a2fb9ab25
stormpsons <mastarmynd@gmail.com>
2022-09-15 13:33:38 -0700

    duel

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  *print_numbers - function that print all numbers from
@@ -8,6 +9,7 @@
 void print_numbers(void)
 {
 	int number = 48;
+
 	for (number = 48; number < 58; number++)
 	{
 		_putchar(number);
