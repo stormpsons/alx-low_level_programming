4f1f50215ba786f6ed97aa98e2cb468e9a61c074
stormpsons <mastarmynd@gmail.com>
2022-09-15 13:17:50 -0700

    yalw

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
-nclude "main.h"
+#include "main.h"
 /**
- *mul - function that multipliestwo numbers
- *@a: first number
- *@b: second number
- *Return: returns result
- */
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
 
 int mul(int a, int b)
-
 {
 return (a * b);
 }
