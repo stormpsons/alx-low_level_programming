eec0e422128483398b7356c6f42bd87c01cfe7eb
stormpsons <mastarmynd@gmail.com>
2022-09-15 15:15:01 -0700

    dust

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,26 +17,31 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+
 		else if ((num % 3) == 0)
 		{
 			printf("Fizz ");
 		}
+
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
 			{
 				printf("Buzz ");
 			}
+
 			else
 			{
 				printf("Buzz");
 			}
 		}
+
 		else
 		{
 			printf("%d ", num);
 		}
 	}
+
 	printf("\n");
 
 return (0);
