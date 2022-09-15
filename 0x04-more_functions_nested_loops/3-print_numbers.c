e7554cd2a27611da72487407ba4ba2620d74ecbc
stormpsons <mastarmynd@gmail.com>
2022-09-15 13:24:57 -0700

    sade

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,10 @@
 /**
  *print_numbers - function that print all numbers from
  *0 to 9
+ *
  *Return: returns nothing
- * /
-
- void print_numbers(void)
+ */
+void print_numbers(void)
 {
 	int number = 48;
 	for (number = 48; number < 58; number++)
