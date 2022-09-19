e03b7249312b6db7a90dcdca208b0d3bf176d7ef
stormpsons <mastarmynd@gmail.com>
2022-09-19 05:15:42 -0700

    go

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+lude "main.h"
+/**
+ *swap_int - swap two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
