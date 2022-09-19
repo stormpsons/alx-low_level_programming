3a8881c8443f56f15e4c1184b54d441a57f67bbb
stormpsons <mastarmynd@gmail.com>
2022-09-19 05:49:42 -0700

    all

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-  *_strlen - returns the length of a string
-  * @s: string
-  *Return: returns lenght;
-  */
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
