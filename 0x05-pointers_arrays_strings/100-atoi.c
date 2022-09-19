92dc8e6c1e729e8d3b2bf179baf104e05d16925d
stormpsons <mastarmynd@gmail.com>
2022-09-19 16:46:25 -0700

    dale

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,10 @@
 /**
- * _atoi - changes a string to an int
- * @s: the string to be changed
- * Return: the converted int
- */
+* _atoi - changes a string to an int
+* @s: the string to be changed
+* Return: the converted int
+*/
 int _atoi(char *s)
+
 {
 	int i = 1;
 
