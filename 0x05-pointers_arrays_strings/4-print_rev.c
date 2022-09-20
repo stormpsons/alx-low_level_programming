32d2c3a9edb1008f33fbb033621091cd0ebbb8bd
stormpsons <mastarmynd@gmail.com>
2022-09-19 17:47:05 -0700

    A

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,3 +15,20 @@ void print_rev(char *s)
 		putchar(*(s + len));
 	putchar(1);
 }
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+	int count, inc;
+
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+
+return (inc);
+}
