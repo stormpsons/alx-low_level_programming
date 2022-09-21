bbea17da27d051eb993c700c5751ebd802f7b79d
stormpsons <mastarmynd@gmail.com>
2022-09-21 12:18:24 -0700

    japa

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,17 @@
 
 char *_strcat(char *dest, char *src)
 {
+	unsigned int i = 0;
+	unsigned int j = 0;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
 
 	return (dest);
 }
