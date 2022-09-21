4724f05fa49048dd0d7335145ade5723c2ab24d6
stormpsons <mastarmynd@gmail.com>
2022-09-21 12:38:32 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,25 @@
 
 char *_strcat(char *dest, char *src)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
+int i;
 
-	while (*(dest + i) != '\0')
-		i++;
-	while (*(src + j) != '\0')
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-	}
+int j;
 
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+	i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+	dest[i] = src[j];
+	i++;
+	j++;
+}
+
+dest[i] = '\0';
+return (dest);
 }
 
 
