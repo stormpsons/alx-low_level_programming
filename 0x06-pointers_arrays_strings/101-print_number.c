007a4b7333099710b285067a147d9ab2e34ff63b
stormpsons <mastarmynd@gmail.com>
2022-09-22 13:19:41 -0700

    days

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,18 @@
 
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int num = n;
 
-n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
 
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
-}
+
