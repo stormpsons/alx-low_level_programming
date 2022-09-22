9cd7c6cce02cf5c8380c8669924e65398c699803
stormpsons <mastarmynd@gmail.com>
2022-09-22 06:33:14 -0700

    soul

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,24 +6,34 @@
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
+
 char *cap_string(char *str)
 {
 int index = 0;
 
-int cspc = 13;
-
-char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
-
-while (s[a])
+while (str[index])
 {
-	i = 0;
-	while (i < cspc)
-	{
-		if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-			s[a] -= 32;
-		i++;
-	}
-	a++;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+
+index++;
 }
-return (s);
+
+return (str);
 }
