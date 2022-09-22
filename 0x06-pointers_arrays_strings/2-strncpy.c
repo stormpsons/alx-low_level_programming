9a9e7fd7868d592c62d0232c2a9ae5bd08e42c05
stormpsons <mastarmynd@gmail.com>
2022-09-22 01:53:28 -0700

    zeal

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,21 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {	
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-for ( ; i < n; i++)
-dest[i] = '\0';
+int a = 0, b = 0;
+
+while (src[b])
+{
+	b++;
+}
+while (a < n && src[a])
+{
+	dest[a] = src[a];
+	a++;
+}
+while (a < n)
+{
+	dest[a] = '\0';
+	a++;
+}
 return (dest);
 }
