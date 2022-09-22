d43dfd0192bfaa846e43390a06b6a01dcc96499a
stormpsons <mastarmynd@gmail.com>
2022-09-22 05:45:14 -0700

    done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,16 +7,15 @@
 *Return: char.
 */
 
-
 char *string_toupper(char *str)
 {
-	int index = 0;
+int index = 0;
 
-	while (str[index++])
-	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
-	}
+while (str[index++])
+{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+}
 
-	return (str);
+return (str);
 }
