bb82b3cb6ac10fe9fc620204df1759e090fabb06
stormpsons <mastarmynd@gmail.com>
2022-09-22 20:48:39 -0700

    lad

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,23 +8,16 @@
 void printnumber(int n)
 {
 
-   if (n < 0) {
-       putchar('-');
-       n = -n;
-   }
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
 
-   if (n == 0)
-      putchar('0');
+    if (n == 0)
+        putchar('0');
 
-   if (n/10)
-      printnumber(n/10);
+    if (n/10)
+        printnumber(n/10);
 
-   putchar(n%10 + '0');
-}
-
-int main(int argc, char** argv)
-{
-   int n = atoi(argv[1]);
-   printnumber(n);
-   printf("\n");
+    putchar(n%10 + '0');
 }
