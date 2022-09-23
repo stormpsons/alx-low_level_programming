64b8780b4c4f75cf9ba9926f9ca793b2fac2a231
stormpsons <mastarmynd@gmail.com>
2022-09-22 21:09:50 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/**
- * void prototypes()
- * int prototypes()
- * char prototypes()
- */
 
 void reverse_array(int *a, int n);
 void print_number(int n);
