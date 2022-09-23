a0459ea9939510812b1ed302cbb7aeefeca75b42
stormpsons <mastarmynd@gmail.com>
2022-09-22 20:42:34 -0700

    dake

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,30 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
-void print_number(int n);
-
+void printnumber(int n)
 {
-unsigned int num = n;
 
-if (n < 0)
-{
-_putchar('-');
-num = -num;
+   if (n < 0) {
+       putchar('-');
+       n = -n;
+   }
+
+   if (n == 0)
+      putchar('0');
+
+   if (n/10)
+      printnumber(n/10);
+
+   putchar(n%10 + '0');
 }
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+
+int main(int argc, char** argv)
+{
+   int n = atoi(argv[1]);
+   printnumber(n);
+   printf("\n");
 }
