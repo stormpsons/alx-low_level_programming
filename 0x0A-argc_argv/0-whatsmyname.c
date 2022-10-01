bbb64a9c36062f4002e7fc6e39b8d6018890e8da
stormpsons <mastarmynd@gmail.com>
2022-10-01 12:37:31 -0700

    solve

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
-
-int main(int argc __attribute__((unused)), char *argv[])
-
+int main(int argc, char *argv[])
 {
-printf("%s\n", *argv);
-
+printf("%s\n", argv[argc * 0]);
 return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "main.h"
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - Program that takes in all integer arguments and returns the sum
+ *  @argc: Number of command line arguments
+ *  @argv: Array name
+ *  Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	int i, d, n, len, f, digit;
+int i, j, length, sum;
+char *ptr;
 
-i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+if (argc < 2)
+printf("0\n");
+else
+{
+sum = 0;
+for (i = 1; i < argc; i++)
+{
+ptr = argv[i];
+length = strlen(ptr);
 
+for (j = 0; j < length; j++)
+{
+if (isdigit(*(ptr + j)) == 0)
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+}
+return (0);
+}
